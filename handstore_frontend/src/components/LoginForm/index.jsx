3e6f544095e48608d7b24f032aedb967686606b4
Ubaid-UllahGitHub <ubaidwaqar71@gmail.com>
2025-04-13 16:23:04 -0400

    commit Changes for login/registration page

--- handstore_frontend/src/components/LoginForm/index.jsx
+++ handstore_frontend/src/components/LoginForm/index.jsx
@@ -1,15 +1,13 @@
-// import { useState } from "react";
-import { Link } from "react-router-dom";
-import { useTheme } from "@mui/material/styles"; // Import the useTheme hook
+// import { useTheme } from "@mui/material/styles"; // Import the useTheme hook
 // import { Box, TextField, Button, Typography, IconButton, InputAdornment, Grid, Container } from "@mui/material";
 // import { Visibility, VisibilityOff } from "@mui/icons-material";
 import React, { useState } from 'react';
+import { Link } from "react-router-dom";
 import {
   TextField,
   Box,
   Button,
   Typography,
-  Icon,
 } from '@mui/material';
 import CheckIcon from '@mui/icons-material/Check';
 
@@ -67,130 +65,6 @@ const LoginForm = () => {
   );
 
   return (
-    // <Box backgroundColor=""
-    //   sx={{
-    //     py: 10,
-    //     // backgroundImage: `url("/image/bgforloginpage.jpg")`, // Background image path
-    //     backgroundSize: "cover",
-    //     backgroundColor: theme => theme.palette.secondary.main,
-    //     backgroundPosition: "center",
-    //   }}>
-    //   <Container maxWidth="lg" sx={{ height: "100%" }}>
-    //     <Grid
-    //       container
-    //       sx={{
-    //         height: "100%",
-    //         display: "flex",
-    //         gap: "0px"
-    //       }}
-    //      >
-    //       {/* Left-colunm */}
-    //       <Grid
-    //         item
-    //         xs={12}
-    //         md={6}
-    //         sx={{
-    //           display: "Flex",              // backgroundImage: `url("/image/loginpageimg.png")`, // Image path
-    //           backgroundSize: "contain",
-    //           justifyContent: "center",
-    //           alignItems: "center",
-    //           backgroundPosition: "center",
-    //           backgroundRepeat: "no-repeat", // Prevent the image from repeating
-    //           height: "400px",
-    //         }}
-    //        >
-    //         <img src="image/loginpageimg.png"
-    //         alt="img"
-    //         width="350px"
-    //         height="auto"
-    //         />
-    //         </Grid>
-    //        {/* Right-colunm */}
-    //        <Grid item
-    //         xs={12}
-    //         md={6}>
-    //        <Grid
-    //         component="form"
-    //         onSubmit={handleLogin}
-    //         sx={{
-    //           maxWidth: 400,
-    //           mx: "auto",
-    //           px: 4,
-    //           py: 4,
-    //           display: "flex",
-    //           flexDirection: "column",
-    //           gap: 4,
-    //           background: "white",
-    //           boxShadow: 1,
-    //           borderRadius: 1,
-    //         }}
-    //        >
-    //         <Box>
-    //           <Typography variant="h5" textAlign="center" fontWeight="bold" color="secondary">
-    //             Login
-    //           </Typography>
-    //           <Typography variant="body" textAlign="center" fontWeight="500" fontSize="12px" color="secondary">
-    //             Don't Have an Account?   <Link to="/signup" style={{ color: "#37acb0", textDecoration: "none" }}>Create Free Account</Link>
-    //           </Typography>
-    //         </Box>
-    //         <TextField
-    //           label="Email or Username"
-    //           variant="outlined"
-    //           fullWidth
-    //           required
-    //           value={emailOrUsername}
-    //           onChange={(e) => setEmailOrUsername(e.target.value)}
-    //         />
-    //         <TextField
-    //           label="Password"
-    //           variant="outlined"
-    //           fullWidth
-    //           required
-    //           type={showPassword ? "text" : "password"}
-    //           value={password}
-    //           onChange={(e) => setPassword(e.target.value)}
-    //           InputProps={{
-    //             endAdornment: (
-    //               <InputAdornment position="end">
-    //                 <IconButton
-    //                   edge="end"
-    //                   onClick={() => setShowPassword((prev) => !prev)}
-    //                 >
-    //                   {showPassword ? <VisibilityOff /> : <Visibility />}
-    //                 </IconButton>
-    //               </InputAdornment>
-    //             ),
-    //           }}
-    //         />
-    //         <Button
-    //           type="submit"
-    //           variant="contained"
-    //           sx={{
-    //             backgroundColor: (theme) => theme.palette.primary.main, // Correctly referencing the theme color
-    //             "&:hover": {
-    //               backgroundColor: (theme) => theme.palette.secondary.main, // Hover effect with primary color
-    //               color: "white"
-    //             },
-    //           }}
-    //         >
-    //           Login
-    //         </Button>
-    //         <Typography variant="body2" textAlign="right">
-    //           <Link
-    //             to="/forgot-password"
-    //             style={{
-    //               color: "#213547", // your secondary.main color manually
-    //               textDecoration: "none",
-    //             }}
-    //           >
-    //             Forgot Password?
-    //           </Link>
-    //         </Typography>
-    //       </Grid>
-    //       </Grid>
-    //     </Grid>
-    //   </Container>
-    // </Box>
     <Box
       sx={{
         width: 380,
